100e777756cfa7c3035326507a8c15ba6fbdc077
Kye <36981621+ProMasterBoy@users.noreply.github.com>
2020-10-08 18:47:14 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.IO;
+namespace NitroGen
+{
+    class Program
+    {
+        static string generated { get; set; }
+        static void Main()
+        {
+            long codes;
+            Console.Title = "Nitro Generator By Kye#5000 | https://github.com/promasterboy";
+        start: Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Discord Nitro Generator by Kye#5000 | https://github.com/promasterboy");
+            Console.WriteLine("");
+            Console.WriteLine("How many Nitro Codes do you want to generate?");
+            try
+            {
+                codes = Convert.ToInt64(Console.ReadLine());
+            }
+            catch
+            {
+                goto start;
+            }
+            Thread thread = new Thread(WriteOut);
+            thread.Start();
+            Console.WriteLine("Generating Discord Nitro Codes, sit back and relax!");
+            for (long i = codes;; i--)
+            {
+                
+                Console.Title = "Nitro Generator By Kye#5000 | https://github.com/promasterboy | " + i + "/" + codes + " Left";
+                generated += "https://discord.gift/" + RandomString(16) + Environment.NewLine;
+                if (i == 0)
+                {
+                    thread.Abort();
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Almost done, please wait!");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Done!");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            
+        }
+
+        static void WriteOut()
+        {
+            while (true)
+            {
+                using (StreamWriter stream = new StreamWriter("codes.txt", append: true))
+                {
+                    stream.Write(generated);
+                    generated = "";
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NitroGen")]
+[assembly: AssemblyDescription("Generate Discord Nitro Codes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kye")]
+[assembly: AssemblyProduct("NitroGen")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("Kye")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("abe24b95-58ae-4705-8aac-45eb634f3d82")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
